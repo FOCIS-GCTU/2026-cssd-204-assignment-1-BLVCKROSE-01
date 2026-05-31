b1c328cb53ae8115c2812f43037bf051be2b6e67
BLVCKROSE <2425403310@live.gctu.edu.gh>
2026-05-31 18:06:31 +0000

    Assignment 1 submission

---
+++ script.py
@@ -0,0 +1,32 @@
+# File: initials.py
+# Description: Print out my initials in stylized large block letters.
+# Assignment Number: 1
+#
+# Name: <Prince Boateng>
+# STUDENT ID:  <2425403310>
+# Email: <2425403310@live.gctu.edu.gh>
+# Grader: <Augustus>
+#
+# On my honor, <Augustus Buckman>, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
+
+def main():
+
+    print()
+    print("...PKB")
+    print()
+    print("..." + "PPPPPPPPPP.." + "..." + ".." + "..." + "KK....KK...." + "..." + ".." + "..." + "BBBBBBBBBB.." + "..." + "..")
+    print("..." + "PP........PP" + "..." + ".." + "..." + "KK...KK....." + "..." + ".." + "..." + "BB........BB" + "..." + "..")
+    print("..." + "PP........PP" + "..." + ".." + "..." + "KK..KK......" + "..." + ".." + "..." + "BB........BB" + "..." + "..")
+    print("..." + "PP........PP" + "..." + ".." + "..." + "KK.KK......." + "..." + ".." + "..." + "BB.......BB" + "..." + "..")
+    print("..." + "PPPPPPPPPP.." + "..." + ".." + "..." + "KKKK........" + "..." + ".." + "..." + "BBBBBBBBBB.." + "..." + "..")
+    print("..." + "PP.........." + "..." + ".." + "..." + "KKKK........" + "..." + ".." + "..." + "BB.......BB" + "..." + "..")
+    print("..." + "PP.........." + "..." + ".." + "..." + "KK.KK......." + "..." + ".." + "..." + "BB........BB" + "..." + "..")
+    print("..." + "PP.........." + "..." + ".." + "..." + "KK..KK......" + "..." + ".." + "..." + "BB........BB" + "..." + "..")
+    print("..." + "PP.........." + "..." + "**" + "..." + "KK...KK....." + "..." + "**" + "..." + "BB........BB" + "..." + "**")
+    print("..." + "PP.........." + "..." + "**" + "..." + "KK....KK...." + "..." + "**" + "..." + "BBBBBBBBBB.." + "..." + "**")
+    print()
+
+
+main()
